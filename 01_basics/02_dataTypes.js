56bed963ea16285058374327614dd262af38b5da
snesh111 <sneshraj111@gmail.com>
2025-11-16 09:24:57 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict " // treat all js as new version
+//alert (2+4) //
+
+let name ="snesh"
+let age = 19
+let isLoggedIn = true
+
+// premetive
+// number  =>  2 to power 53
+// bigInt => 74786436n 
+// string => "", ' '
+// boolean => true / false
+// null => stand alone value initily 
+// undefined => variable is declared but not given value 
+// symbol => unique
+
+//reference
+
+// object 
+
+console.log(typeof "snesh" );
