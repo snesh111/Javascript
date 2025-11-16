d9f5cb28c60004601dc0a137ac05d8f71c7ba315
snesh111 <sneshraj111@gmail.com>
2025-11-16 11:01:49 +0000

    comparisonops

---
+++ 01_basics/03_convertionOperation.js
@@ -0,0 +1,71 @@
+//rollno = "12"
+
+//console.log(typeof rollno);
+
+//let valueInNumber = Number(rollno)
+//console.log( typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "34kjfd" => Nan
+// true => 1  f => 0
+
+
+//let isLoggedIn = "snesh"
+
+//let bolleanisLoggedIn = Boolean (isLoggedIn)
+//console.log( bolleanisLoggedIn);
+
+// 1 =>  ture 
+// 0 => false
+// " " => false
+// "snesh" => true
+
+// let sameNumber = 23
+//let stringNumber = String (sameNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+// operations //
+
+//let postiveValue = 12
+//let negValue = -postiveValue
+//console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2/10);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2-3);
+// console.log(2%5);
+
+
+// let str1 = "snesh"
+// let str2 = "raj "
+// let str3 = str1 + str2
+//console.log(str3);
+
+// console.log(1+2);
+// console.log("1"+2);
+// console.log(1+"3");
+// console.log(2+ 3+ "3");
+// console.log("1"+2+2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
