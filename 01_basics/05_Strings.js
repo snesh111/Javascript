cec5991be5bad7def6af4ea1df91eb2efdb40185
snesh111 <sneshraj111@gmail.com>
2025-11-16 14:38:42 +0000

    string

---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,30 @@
+ const myName = "snesh"
+ const rollno = 23
+
+console.log(`my name is ${myName} and my rollno is ${rollno}`);
+
+
+const gamerName = new String ('hello learning string in js')
+console.log(gamerName[0]);
+console.log(gamerName.toUpperCase());
+console.log(gamerName.charAt(0));
+console.log(gamerName.indexOf('o'));
+
+
+const newstring = gamerName.substring(0,4)
+console.log(newstring);
+
+const anotherString = gamerName.slice (-8,2)
+console.log(anotherString);
+
+const name1 = "    raju  "
+console.log(name1);
+console.log(name1.trim());
+
+
+const url = "https://snesh.com/jhd%jjdhraj"
+console.log(url.replace('com','comm'));
+console.log(url.includes('snessh'));
+
+
+
