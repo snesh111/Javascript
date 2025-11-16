75221211f75aa0853a2736406dadb92dba7b52a1
snesh111 <sneshraj111@gmail.com>
2025-11-16 12:38:02 +0000

    added

--- 01_basics/dataTypes-revision.js
+++ 01_basics/dataTypes-revision.js
@@ -8,15 +8,47 @@
 // const emailId = null 
 // let name;
 
-let id = Symbol("123")
-let numId = Symbol ("123")
-console.log( id == numId);
-
-
-
+// let id = Symbol("123")
+// let numId = Symbol ("123")
+// console.log( id == numId);
 
 
 
 // reference type non- primitive 
-
 // Array,objects,functions
+
+// const myfunction = function ()
+// {
+
+// }
+
+//  const array=["snesh" ,"dfjhdfh" ]
+
+
+
+//stack (primitive)
+
+// let myName = "snesh"
+// let anotherName= myName
+// anotherName = "chinnu"
+// console.log(myName);
+
+
+
+
+
+let user1 = {
+    userEmail : "raja@gmail.com",
+    uId : 18
+}
+console.log(user1);
+
+let user2 = user1
+console.log(user2);
+
+user2.userEmail = "user2.gmail.com"
+console.log(user2.userEmail);
+console.log(user1.userEmail);
+
+console.log(user1);
+
