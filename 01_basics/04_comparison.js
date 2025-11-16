a7589294f4921287e7918cc7a78cf36c1f9e346c
snesh111 <sneshraj111@gmail.com>
2025-11-16 11:18:32 +0000

    ops

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,11 @@
+// console.log(1 > 2);
+// console.log(2>=2);
+// console.log(3 == 3);
+// console.log(2 != 2);
+
+// console.log(typeof null );
+// console.log(null >= 0);
+
+
+// console.log("2" >= 2);
+
