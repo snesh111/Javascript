2a3e4e21a25984d21cf5bee143313a79a5779436
snesh111 <sneshraj111@gmail.com>
2025-11-16 10:27:05 +0000

    convertionops

---
+++ 01_basics/convertionOperation.js
@@ -0,0 +1,33 @@
+rollno = "12"
+
+console.log(typeof rollno);
+
+let valueInNumber = Number(rollno)
+console.log( typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "34kjfd" => Nan
+// true => 1  f => 0
+
+
+//let isLoggedIn = "snesh"
+
+//let bolleanisLoggedIn = Boolean (isLoggedIn)
+//console.log( bolleanisLoggedIn);
+
+// 1 =>  ture 
+// 0 => false
+// " " => false
+// "snesh" => true
+
+// let sameNumber = 23
+//let stringNumber = String (sameNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+
+
+
