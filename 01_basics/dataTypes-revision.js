8b64e1af799d3b92d818c362ab5337a5baddbfec
snesh111 <sneshraj111@gmail.com>
2025-11-16 11:43:38 +0000

    revision

---
+++ 01_basics/dataTypes-revision.js
@@ -0,0 +1,22 @@
+//primitive
+// call by value copy of the og value all the changes  will happen in copy 
+// 7 types -- string,number,boolean,undefined,null,symbol,BigInt
+
+// const score = 100
+// const scoreNumber = 123.1
+// const isLoggedIn = true
+// const emailId = null 
+// let name;
+
+let id = Symbol("123")
+let numId = Symbol ("123")
+console.log( id == numId);
+
+
+
+
+
+
+// reference type non- primitive 
+
+// Array,objects,functions
