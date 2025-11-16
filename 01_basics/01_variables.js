08b710f228ea38335ec85ce96c25890adc03f324
snesh111 <sneshraj111@gmail.com>
2025-11-16 09:24:43 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 9092
+let accountEmail = "snesh@gmail.com"
+var accountPass = "2334"
+accountCity = "mumbai"
+
+// accountId = 23 // not allowed
+console.log(accountId);
+
+accountEmail = "car@gmail.com"
+accountPass = "21"
+accountCity = "pune"
+
+
+/*
+prefer not to use var -
+*/
+
+console.table ([accountId,accountEmail,accountPass,accountCity])
+
+
+
+
