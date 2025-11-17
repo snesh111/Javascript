fb611e834ed389a4b98169dd2fa2cae2ea1355d7
snesh111 <sneshraj111@gmail.com>
2025-11-17 15:48:30 +0000

    4

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,45 @@
+// const instaName = new Object()
+// console.log(instaName);       /// single tone object
+
+
+// const user = {}
+// user.email = "raj@gmail.com",
+// user.Name = "snesh"
+// console.log(user);
+
+// const XACCOUNt = {
+//     userEM : "raj@gamil.com",
+//     userName : {
+//         userFullName : {
+//             firstNAme : "snesh",
+//             middleNAme :"usbbiah",
+//             LAstNAme :"raj"
+//         }
+//     }
+// }
+// console.log(XACCOUNt.userName.userFullName.LAstNAme);
+
+
+// const Obje1 = {1:"r",2:"w"}
+// const Obje2 = {3:"r",4:"w"}
+
+// const obje3 = {...Obje1,...Obje2}
+// console.log(obje3);
+
+
+const accHolder =[
+
+        {
+            id1: 1,
+            NAme:"sneh"
+        },
+        {
+            id2: 2,
+            NAme:"sneh"
+        },
+        {
+            id3: 3,
+            NAme:"sneh"
+        },
+]
+console.log(accHolder[2].id3)  
