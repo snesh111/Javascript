cff574e677f1caa0528dd5509ae1f01b2dd3dc90
snesh111 <sneshraj111@gmail.com>
2025-11-17 08:07:13 +0000

    maths

---
+++ 01_basics/06_math_numbers.js
@@ -0,0 +1,37 @@
+// numbers
+
+// const balance = 300
+// console.log(balance);
+
+// const score = new Number (3000)
+// console.log(score);
+
+
+// console.log(score.toString().length);
+// console.log(score.toFixed(2));
+
+// const otherNumber = 12.30
+// console.log(otherNumber.toPrecision(2));
+
+// const thousands = 100000
+// console.log(thousands.toLocaleString('en-IN'));
+
+
+// maths
+// console.log(Math.abs(-9));
+// console.log(Math.round(4.4));
+// console.log(Math.sqrt(36));
+// console.log(Math.min(3,3,5,6,7,8,5));
+// console.log(Math.max(3,3,5,6,7,8,5));
+
+console.log(Math.random())
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max-min+1)+10));
+
+
+
+
+
