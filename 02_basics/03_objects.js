9c1d0d7fdd371f2ffaa3b5cb58c9d294983813fb
snesh111 <sneshraj111@gmail.com>
2025-11-17 13:12:08 +0000

    objs1

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,12 +1,34 @@
 // object literals 
-
+const mysym=Symbol("key1")
 const user = {
     name :"snesh",
+    [mysym] : "key1",
     age : 18,
     emailid : "raju@gamil.com",
     isLoggedIn : false,
     lastLoggedIn : ["Monday","sunday"]
 
 }
-console.log(user.age)
-console.log(user["emailid"]);
+// console.log(user.age)
+// console.log(user["emailid"]);
+// console.log(user[mysym]);
+
+// console.log(user.emailid = "raj@gmail.com")
+// // Object.freeze(user)
+// console.log(user.emailid = "rajshakar@gmail.com")
+// console.log(user.emailid)
+
+user.greeting  = function()
+{
+console.log("helloo");
+
+}
+user.greetingTwo  = function()
+{
+console.log(`hello  user ${this.name}`);
+
+}
+
+console.log(user.greeting());
+console.log(user.greetingTwo());
+
