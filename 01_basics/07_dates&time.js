30b33a74094816268efcb88e8a3e1be199d07db5
snesh111 <sneshraj111@gmail.com>
2025-11-17 10:34:49 +0000

    done

---
+++ 01_basics/07_dates&time.js
@@ -0,0 +1,12 @@
+const date = new Date();
+console.log(date);
+
+
+// we can also pass arguments to set time as we want 
+// date(year,month,day,hour,minutes,seconds,ms)
+
+const year = date.getFullYear();
+console.log(year);
+
+const month = date.getMonth();
+console.log(month);
---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,33 @@
+const myArray=[0,1,2,3,4]
+// console.log(myArray[1]);
+console.log(myArray);
+
+
+// myArray.push (45)
+// console.log(myArray);
+
+// myArray.pop()
+// console.log(myArray);
+
+// myArray.unshift(20)
+// myArray.shift()
+// console.log(myArray);
+
+// myArray.shift()
+// console.log(myArray);
+
+// console.log(myArray.includes(34))
+// console.log(myArray.indexOf(34));
+
+// newarray= myArray.join()
+// console.log( typeof newarray);
+
+//slice //splice
+const mya1 = myArray.slice(1,3)
+console.log( "A", mya1); 
+
+//splice
+const myarray2 = myArray.splice(1,2)
+console.log(myarray2);
+
+console.log(myArray)
