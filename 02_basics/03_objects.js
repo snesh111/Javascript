7448d5981fa3f230767fbc0d21ad0b43444d5b90
snesh111 <sneshraj111@gmail.com>
2025-11-17 11:30:02 +0000

    obj

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,12 @@
+// object literals 
+
+const user = {
+    name :"snesh",
+    age : 18,
+    emailid : "raju@gamil.com",
+    isLoggedIn : false,
+    lastLoggedIn : ["Monday","sunday"]
+
+}
+console.log(user.age)
+console.log(user["emailid"]);
