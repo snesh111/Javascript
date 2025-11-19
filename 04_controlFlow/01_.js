bd2b0e8861e1e4e7369853f13ffedb7c092389df
snesh111 <sneshraj111@gmail.com>
2025-11-19 15:58:34 +0000

    condition

---
+++ 04_controlFlow/01_.js
@@ -0,0 +1,101 @@
+//if
+// if (conditon)
+// {
+
+// }
+
+// if (true) // 100% goes inside 
+// {
+
+// }
+
+//comapresion 
+
+// <,>,<=,>=,=== to strict it datatype also,==,!=,!==
+
+// if(2=="2")
+// {
+//     console.log("you are right");      /// in this situation we use === to check strictly
+    
+// }
+
+// if(2==="2")
+// {
+//     console.log("rightu");    
+// }
+// else
+// {
+//     console.log("wrong");
+    
+// }
+
+// let temp=17
+// if( temp <=17 ){
+//             console.log("17 degree");
+// }
+// else
+// {
+//     console.log("temp is greater than 20 degree");
+    
+
+// }
+
+
+// if -else-if
+
+// const score=70
+
+// if( score > 90){
+//     console.log("A");
+// }
+// else if(score >75){
+//         console.log("B");
+// }
+// else if(score > 60)
+// {
+//     console.log("C");
+    
+// }
+// else
+// {
+//     console.log("fail");
+    
+// }
+
+// && and opr both should be true then only goes inside side
+
+// const userEmail = true
+// const userotp = true  
+
+// if(userEmail && userotp){
+//             console.log("succesful");
+// }
+// else  // both the statement should be true then only goes inside if it is not true it goes inside the else part
+// {
+//     console.log("retry agian enter otp");
+    
+// }
+
+
+// || or  in both the conditions one should be true 
+
+const userName = true
+const userlocacces= false
+
+if ( userName || userlocacces){
+            console.log("u can login");
+}
+else
+{
+    console.log("retry give any of the info");
+    
+}
+
+
+
+
+
+
+
+
+
---
+++ 04_controlFlow/02_switch.js
@@ -0,0 +1,28 @@
+//switch -case
+
+const month =3
+switch (month) {
+    case 1:
+        console.log("jan");
+        break;
+    
+    case 2:
+        console.log("feb");
+        break;
+    
+    case 3:
+        console.log("mar");
+        break;
+    
+    case 4:
+        console.log("ap");
+        break;
+    
+    case 5:
+        console.log("may");
+        break;
+        
+    default:
+        console.log("something went wrong");
+        break;
+}
