52f4d577cd8d11e1c27d4902a0c7f57970c11be4
snesh111 <sneshraj111@gmail.com>
2025-11-20 11:22:02 +0000

    loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,17 @@
+//for loop
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     console.log(element);
+// }
+
+//for loop using array
+
+const myArray=["snesh","raju","lokesh"]
+for(i=0;i<myArray.length;i++){
+
+    const element =myArray[i];
+    console.log(element);
+    
+        
+}
