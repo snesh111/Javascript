a0a11eba4d1f6516361e4d97790beebb43139c7f
snesh111 <sneshraj111@gmail.com>
2025-11-20 10:41:01 +0000

    truthy

---
+++ 04_controlFlow/03_truthy.js
@@ -0,0 +1,40 @@
+// const userName = []  // if it has value than it prints // try array    
+
+// if (userName)
+// {
+//     console.log("true u have");
+    
+// }
+// else
+// {
+//     console.log("false u dont have");
+    
+// }
+
+
+// falsy - values 
+// bigInt 7348648n , false ,0,-0,null,undefined,Nan,""
+
+// truthy values 
+// " " , " false" "0",{},[],function (){}
+
+// ?? use  cases only for undifined and null 
+
+// let value1
+// value1 =5 ?? 10
+// console.log(value1);
+
+// value1=null ?? 20
+// console.log(value1);
+
+// value1=undefined ?? 23
+// console.log(value1);
+
+
+
+// ternary operator
+
+const ShoePrice=400
+ShoePrice<=400 ? console.log("less than 400") : console.log("more than 400");
+
+
